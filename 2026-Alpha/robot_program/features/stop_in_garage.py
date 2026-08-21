0dc07613b5911b6b53f2157b8c1c9bfb59801eb3
Reina Orita <orita.reina.oo@gmail.com>
2026-08-21 21:45:33 +0900

    スタートとゴール 8月21日

--- 2026-Alpha/robot_program/features/stop_in_garage.py
+++ 2026-Alpha/robot_program/features/stop_in_garage.py
@@ -7,5 +7,12 @@ from ..placeholder import PendingFeature
 def build_stop_in_garage(context, config):
     # No.20 ガレージ内停止と停止保持を担当する。
     root = Sequence(name="stop_in_garage", memory=True)
-    root.add_children([PendingFeature(name="stop_in_garage_pending")])
+
+    #ゴール01（ライントレース）
+    goal_01 = Parallel(name="goal 01", policy=ParallelPolicy.SuccessOnOne())
+
+    #ゴール02（ジャイロ走行）
+    goal_02 = Parallel(name="goal 02", policy=ParallelPolicy.SuccessOnOne())
+
+    root.add_children([goal_01,goal_02])
     return root
