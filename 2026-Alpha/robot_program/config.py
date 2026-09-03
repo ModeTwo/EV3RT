806d7d4e6a9a78cb9804cdac0c1d119b080acb3a
shunsuke1014 <shunsuke.s1014@gmail.com>
2026-09-03 21:38:31 +0900

    config

--- 2026-Alpha/robot_program/config.py
+++ 2026-Alpha/robot_program/config.py
@@ -6,10 +6,10 @@ from dataclasses import dataclass
 @dataclass(frozen=True)
 class RaceConfig:
     # 試験時に工程単位で有効・無効を切り替えられるよう、設定値を一か所へ集約する。
-    lapgate : bool = True
-    enable_bottle_delivery: bool = True
+    lapgate : bool = False
+    enable_bottle_delivery: bool = False
     enable_et_rally: bool = True
     et_rally_laps: int = 3
-    enable_et_sumo: bool = True
-    enable_finish: bool = True
+    enable_et_sumo: bool = False
+    enable_finish: bool = False
 
